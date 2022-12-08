c53509d9a6aa470e9a363a17cab16a2c47790cde
sanji92 <abina.habib93@gmail.com>
2022-12-08 13:19:12 +0100

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'I Am Rich',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Center(
+            child: Text('I Am Rich'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image.asset("images/diamond.png"),
+        ),
+      ),
+    );
+  }
+}
